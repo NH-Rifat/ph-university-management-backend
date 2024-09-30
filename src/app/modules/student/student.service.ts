8d85f2f1f0b43a5c733a8277bad97bcd064053c1
Hasan Rifat <rifatr729@gmail.com>
2024-09-30 13:36:47 +0600

    implemented transaction in between deletion of user and student

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,4 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import mongoose from "mongoose";
 import { studentModel } from "./student.model";
+import { UserModel } from "../user/user.model";
 
 const getAllStudentsFromDB = async () => {
   const result = await studentModel
@@ -13,8 +17,39 @@ const getAllStudentsFromDB = async () => {
   return result;
 };
 const deleteStudentByIdFromDB = async (id: string) => {
-  const result = await studentModel.updateOne({ id }, { isDeleted: true });
-  return result;
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+
+    const deletedStudent = await studentModel.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      {
+        new: true,
+        session,
+      }
+    );
+    if (!deletedStudent) {
+      throw new Error("Student not found");
+    }
+    const deletedUser = await UserModel.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      {
+        new: true,
+        session,
+      }
+    );
+    if (!deletedUser) {
+      throw new Error("User not found");
+    }
+    await session.commitTransaction();
+    await session.endSession();
+    return deletedStudent;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession;
+  }
 };
 
 const getStudentByIdFromDB = async (id: string) => {
