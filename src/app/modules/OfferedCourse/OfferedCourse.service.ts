154eef304f8cb1e93bc98b9c409b74235d4c1106
Hasan Rifat <rifatr729@gmail.com>
2024-10-24 11:11:25 +0600

    offered couse module initiated with corresponding file

---
+++ src/app/modules/OfferedCourse/OfferedCourse.service.ts
@@ -0,0 +1,22 @@
+import { TOfferedCourse } from "./OfferedCourse.interface";
+
+const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {};
+
+const getAllOfferedCoursesFromDB = async (query: Record<string, unknown>) => {};
+
+const getSingleOfferedCourseFromDB = async (id: string) => {};
+
+const updateOfferedCourseIntoDB = async (
+  id: string,
+  payload: Pick<TOfferedCourse, "faculty" | "days" | "startTime" | "endTime">
+) => {};
+
+const deleteOfferedCourseFromDB = async (id: string) => {};
+
+export const OfferedCourseServices = {
+  createOfferedCourseIntoDB,
+  getAllOfferedCoursesFromDB,
+  getSingleOfferedCourseFromDB,
+  deleteOfferedCourseFromDB,
+  updateOfferedCourseIntoDB,
+};
